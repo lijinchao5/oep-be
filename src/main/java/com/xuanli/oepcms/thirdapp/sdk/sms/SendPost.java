e21068aef0946989cc72a88654dc31e20417706e
wang zhengbo <wangzb4@lenovo.com>
2018-02-03 18:08:36 +0800

    [wangzhengbo] #N/A 删除短信测试代码。

--- src/main/java/com/xuanli/oepcms/thirdapp/sdk/sms/SendPost.java
+++ src/main/java/com/xuanli/oepcms/thirdapp/sdk/sms/SendPost.java
@@ -1,20 +1,4 @@
 ﻿package com.xuanli.oepcms.thirdapp.sdk.sms;
 
 public class SendPost {
-//	public static void main(String[] args) {
-//		SendSms post = new SendSms();
-//		String action = "send"; // 表示发送短信
-//		String account = "922022"; // 用户名10690022
-//		String password = "5rtwzC"; // 密码
-//		String mobile = "18210729410"; // 合法的手机号码，号码间用英文逗号分隔
-//		int code = (int) ((Math.random() * 9 + 1) * 100000);
-//		String content = "【AI英语】验证码是" + code + "您正在改密码验证码30分钟有效";// 发送内容
-//		String extno = "10690022"; // 为通道扩展子号码，可以为空
-//		String rt = "json";
-//		String parm = "action=" + action + "&account=" + account + "&password=" + password + "&mobile=" + mobile + "&content=" + content + "&extno=" + extno + "&rt=" + rt;
-//		String host = "http://47.100.188.160:7862/sms"; // 提交地址ַ
-//		// 发送 POST 请求
-//		String sr = post.sendPost(host, parm);
-//		System.out.println(sr);
-//	}
 }
