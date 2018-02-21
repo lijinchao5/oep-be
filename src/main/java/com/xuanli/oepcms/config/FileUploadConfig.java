4a135a2a9ad4d05defa9dc3cd2c79a8cc7aa04fc
wang zhengbo <wangzb4@lenovo.com>
2018-02-21 17:04:49 +0800

    [wangzhengbo] #N/A 增加文件上传multipartResolver配置。

---
+++ src/main/java/com/xuanli/oepcms/config/FileUploadConfig.java
@@ -0,0 +1,23 @@
+package com.xuanli.oepcms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+
+@Configuration
+public class FileUploadConfig {
+    // 上传文件允许的最大大小
+    private static final int MAX_SIZE = 2 * 1024 * 1024; // 2MB
+
+    @Bean
+    public MultipartResolver multipartResolver() {
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+
+        multipartResolver.setMaxUploadSize(MAX_SIZE);
+        multipartResolver.setMaxInMemorySize(MAX_SIZE);
+        multipartResolver.setMaxUploadSize(MAX_SIZE);
+
+        return multipartResolver;
+    }
+}
