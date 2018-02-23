2c3bb6a6e233572334069731d87e45d2196cd353
lijinchao <lijinchao5@163.com>
2018-02-23 10:58:33 +0800

    [lijinchao] 同步接口

--- src/main/java/com/xuanli/oepcms/controller/SynchronizeController.java
+++ src/main/java/com/xuanli/oepcms/controller/SynchronizeController.java
@@ -18,6 +18,10 @@ import com.xuanli.oepcms.entity.UserEntity;
 import com.xuanli.oepcms.service.UserService;
 import com.xuanli.oepcms.vo.RestResult;
 
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+
 /**
  * @author lijinchao
  * @date 2018年2月7日 下午5:55:10
@@ -28,6 +32,7 @@ public class SynchronizeController extends BaseController{
 	@Autowired
 	private UserService userService;
 	
+	@ApiOperation(value = "同步获取用户信息", notes = "同步获取用户信息方法")
 	@RequestMapping(value = "getUsers.do", method = RequestMethod.GET)
 	public RestResult<List<UserEntity>> getUsers(){
 		List<UserEntity> UserEntities = userService.getUsers();
@@ -40,6 +45,8 @@ public class SynchronizeController extends BaseController{
 	 * @param userEntity
 	 * @return
 	 */
+    @ApiOperation(value = "同步增加用户", notes = "同步增加用户方法")
+    @ApiImplicitParams({ @ApiImplicitParam(name = "userInfo", value = "用户信息", required = true, dataType = "String") })
 	@RequestMapping(value = "saveUser.do", method = RequestMethod.POST)
 	public String saveUser(@RequestBody String userInfo){
 		UserEntity userEntity = JSONObject.parseObject(userInfo,UserEntity.class);
@@ -57,6 +64,9 @@ public class SynchronizeController extends BaseController{
 			return "1";
 		}
 	}
+    
+    @ApiOperation(value = "同步修改用户信息", notes = "同步修改用户信息方法")
+    @ApiImplicitParams({ @ApiImplicitParam(name = "userInfo", value = "用户信息", required = true, dataType = "String") })
 	@RequestMapping(value = "updateUser.do", method = RequestMethod.PUT)
 	public String updateUser(@RequestBody String userInfo){
 		UserEntity userEntity = JSONObject.parseObject(userInfo,UserEntity.class);
@@ -74,6 +84,8 @@ public class SynchronizeController extends BaseController{
 		}
 	}
 	
+    @ApiOperation(value = "同步删除用户信息", notes = "逻辑删除用户方法")
+    @ApiImplicitParams({ @ApiImplicitParam(name = "userId", value = "用户信息", required = true, dataType = "Long") })
 	@RequestMapping(value = "deleteUser.do", method = RequestMethod.DELETE)
 	public RestResult<String> deleteUser(Long userId){
 		if (null == userId) {
