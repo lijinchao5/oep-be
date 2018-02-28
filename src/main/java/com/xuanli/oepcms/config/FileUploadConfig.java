ba7ebe614594b77667af12080d983d42be3ee315
qiaoshipeng <qiaoshipeng>
2018-02-28 09:19:51 +0800

    修改上传信息

--- src/main/java/com/xuanli/oepcms/config/FileUploadConfig.java
+++ src/main/java/com/xuanli/oepcms/config/FileUploadConfig.java
@@ -10,14 +10,14 @@ public class FileUploadConfig {
     // 上传文件允许的最大大小
     private static final int MAX_SIZE = 2 * 1024 * 1024; // 2MB
 
-    @Bean
-    public MultipartResolver multipartResolver() {
-        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
-
-        multipartResolver.setMaxUploadSize(MAX_SIZE);
-        multipartResolver.setMaxInMemorySize(MAX_SIZE);
-        multipartResolver.setMaxUploadSize(MAX_SIZE);
-
-        return multipartResolver;
-    }
+//    @Bean
+//    public MultipartResolver multipartResolver() {
+//        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+//
+//        multipartResolver.setMaxUploadSize(MAX_SIZE);
+//        multipartResolver.setMaxInMemorySize(MAX_SIZE);
+//        multipartResolver.setMaxUploadSize(MAX_SIZE);
+//
+//        return multipartResolver;
+//    }
 }
