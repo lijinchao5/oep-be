3a59c996e4ae350c0a1f4649d1474132a45373af
qiaoshipeng <qiaoshipeng>
2018-03-09 17:20:04 +0800

    111

--- src/main/java/com/xuanli/oepcms/service/PaperService.java
+++ src/main/java/com/xuanli/oepcms/service/PaperService.java
@@ -56,9 +56,6 @@ public class PaperService extends BaseService {
 		requestMap.put("start", pageBean.getRowFrom());
 		requestMap.put("end", pageBean.getPageSize());
 		List<Map<String, Object>> resultMap = paperEntityMapper.findPaperByPage(requestMap);
-		for (Map<String, Object> map : resultMap) {
-			System.out.println(map.get("id"));
-		}
 		pageBean.setRows(resultMap);
 	}
 
