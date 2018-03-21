aec16682cabc8aa9bb71d733b9db9cbd6507f06f
lijinchao <lijinchao5@163.com>
2018-03-21 18:20:40 +0800

    [lijinchao] 修改提示语

--- src/main/java/com/xuanli/oepcms/controller/ExamController.java
+++ src/main/java/com/xuanli/oepcms/controller/ExamController.java
@@ -51,7 +51,7 @@ public class ExamController extends BaseController {
 	public RestResult<String> genteratorExam(String name, String notice, String classIds, Date startTime, Date endTime, Long paperId) {
 		Date now = new Date();
 		if (startTime.getTime() < now.getTime() || startTime.getTime() > endTime.getTime()) {
-			return failed(ExceptionCode.PARAMETER_VALIDATE_ERROR_CODE, "开始时间应大于当前时间并小于结束时间!");
+			return failed(ExceptionCode.PARAMETER_VALIDATE_ERROR_CODE, "开始时间不能小于当前时间");
 		}
 		try {
 			Long userId = getCurrentUser().getId();
