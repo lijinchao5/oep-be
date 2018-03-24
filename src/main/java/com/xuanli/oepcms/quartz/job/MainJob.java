c0ffa79b04fa982c01f0cb928bd2888920896e80
qiaoshipeng <qiaoshipeng>
2018-03-24 10:48:01 +0800

    1232

--- src/main/java/com/xuanli/oepcms/quartz/job/MainJob.java
+++ src/main/java/com/xuanli/oepcms/quartz/job/MainJob.java
@@ -5,19 +5,14 @@ import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.xuanli.oepcms.config.ApplicationContextProvider;
-import com.xuanli.oepcms.service.ReportService;
-
 public class MainJob implements BaseJob{
-	private static Logger _log = LoggerFactory.getLogger(MainJob.class);
+	private static Logger logger = LoggerFactory.getLogger(MainJob.class);
 
 	public MainJob() {
 
 	}
 
 	public void execute(JobExecutionContext context) throws JobExecutionException {
-		ReportService reportService = ApplicationContextProvider.getApplicationContext().getBean(ReportService.class);
-		System.out.println(reportService);
-		_log.info("主要的定时任务!");
+		logger.info("主要的定时任务!");
 	}
 }
